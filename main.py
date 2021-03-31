ed42d2a6813bce2ad0f008e9f3d5a70b4b7a10ea
Kevsnz <15689588+Kevsnz@users.noreply.github.com>
2021-03-31 23:14:39 +0500

    More constants and more logging. Model saving

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ import datetime
 import os
 import tensorboard
 
-exp_capacity = 100000
+exp_capacity = 10000
 epsilon_initial = 1.0
-epsilon_final = 0.02
+epsilon_final = 0.05
 epsilon_decay_time = 1000000
 epsilon_decay_amount = epsilon_initial - epsilon_final
-initial_exp_gathering = 10000
+initial_exp_gathering = 2000
 batch_size = 16
-epochs = 8
+epochs = 32
 gamma = 0.95
+targetSyncInterval = 2000
+learningRate = 0.0002
 
 def silentPlayout(game):
     turn = 0
@@ -66,11 +68,12 @@ def playAndLearn(agentNet, targetNet, player):
     sampleCount = 0
     sampleLast = 0
     episodeCount = 0
+    lastSyncSample = 0
     timeLast = time.perf_counter()
     epsilon = epsilon_initial
 
     lossFunc = torch.nn.MSELoss()
-    optim = torch.optim.Adam(agentNet.parameters(), 0.001)
+    optim = torch.optim.Adam(agentNet.parameters(), lr=learningRate)
 
     stepCount = 0
     epCount = 0
@@ -107,6 +110,7 @@ def playAndLearn(agentNet, targetNet, player):
             if sampleCount < initial_exp_gathering:
                 continue
 
+            lossAvg = 0
             for _ in range(epochs):
                 states, actions, rewards, terms, newStates = expBuffer.sample(batch_size)
                 states_t = agentNet.prepareInputs(states) #torch.Tensor(agentNet.prepareInput(s) for s in states).to(device)
@@ -127,12 +131,19 @@ def playAndLearn(agentNet, targetNet, player):
                 loss.backward()
                 optim.step()
 
-                if episodeCount % 5 == 4:
+                if sampleCount - lastSyncSample > targetSyncInterval:
+                    lastSyncSample += targetSyncInterval
                     targetNet.load_state_dict(agentNet.state_dict())
+                
+                lossAvg += loss.item()
+            
+            writer.add_scalar('Loss', lossAvg/epochs, sampleCount)
 
 
     except KeyboardInterrupt:
         print(f'Playing stopped after {sampleCount} steps ({episodeCount} episodes).')
+        torch.save(agentNet.state_dict(), os.path.join('models', datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')+f' SC {sampleCount}'))
+
     
     writer.close()
 
