0638cf6c1d4c312859dfd00825475eb660957d0a
Kevsnz <15689588+Kevsnz@users.noreply.github.com>
2021-04-23 19:18:08 +0500

    Limit episode length, make ending with invalid swipe optional

--- agent_player.py
+++ agent_player.py
@@ -5,13 +5,18 @@ from agent_net import AgentNet
 from game import Game2048
 
 class AgentPlayer:
+    TURN_LIMIT = 2 ** Game2048.TARGET_SCORE
+    MAX_SEQ_INVALID = 4
+
     def __init__(self, net: AgentNet, game: Game2048):
         self.net = net
         self.game = game
+        self.invalidSeq = 0
     
     
-    def playEpisode(self):
+    def playEpisode(self, invalidEnd = False):
         self.game.reset()
+        self.invalidSeq = 0
 
         states = []
         actions = []
@@ -19,7 +24,7 @@ class AgentPlayer:
         endeds = []
         newStates = []
         while True:
-            s, a, r, e, s1 = self._makeTurn(self.net, self.game)
+            s, a, r, e, s1 = self._makeTurn(self.net, self.game, invalidEnd)
 
             states.append(s)
             actions.append(a)
@@ -33,19 +38,28 @@ class AgentPlayer:
         return states, actions, rewards, endeds, newStates
     
 
-    def makeTurn(self):
-        return self._makeTurn(self.net, self.game)
+    def makeTurn(self, invalidEnd = False):
+        return self._makeTurn(self.net, self.game, invalidEnd)
     
 
-    def _makeTurn(self, net: AgentNet, game: Game2048):
+    def _makeTurn(self, net: AgentNet, game: Game2048, invalidEnd = False):
         state = game.board.copy()
         action = net(net.prepareInputs(state))[0].squeeze(0)
         action = torch.softmax(action, dim=0).detach().numpy()
 
         dir = np.random.choice(len(action), size=1, p=action)
         reward, ended, valid = game.swipe(dir)
-        if not valid:
+        if valid:
+            self.invalidSeq = 0
+        else:
             reward = -20
+            self.invalidSeq += 1
+            ended = invalidEnd
+            if self.invalidSeq == self.MAX_SEQ_INVALID:
+                ended = True
+                self.invalidSeq = 0
+        
+        if game.swipeCount >= self.TURN_LIMIT:
             ended = True
         
         return state, dir, reward, ended, game.board.copy()
